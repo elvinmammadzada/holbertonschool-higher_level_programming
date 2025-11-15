a0932a4d89dfa4678859e4b81a1a99a78f7d584f
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-15 17:27:00 +0000

    s

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100*100):
-    a = i // 100
-    b = i % 100
-    if a < b:
-        if a == 98 and b == 99:
-            print("{:02d}, {:02d}".format(a, b))
+for i in range(0, 10):
+    for j in range(i+1, 10):
+        if i != 8:
+            print("{:01d}{:01d}".format(i, j), end=", ")
         else:
-            print("{:02d}, {:02d}".format(a, b), end=", ")
+            print("{:01d}{:01d}".format(i, j))
