c41eab79ca20fe217293af88d9cd9821e507167a
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-15 17:20:49 +0000

    s

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100*100):
-    a = i // 100
-    b = i % 100
-    if a < b:
-        if a == 98 and b == 99:
-            print("{:02d}, {:02d}".format(a, b))
-        else:
-            print("{:02d}, {:02d}".format(a, b), end=", ")
+for i in range(0, 100):
+    print("{:02d}".format(i), end=", " if i != 99 else "\n")
 
