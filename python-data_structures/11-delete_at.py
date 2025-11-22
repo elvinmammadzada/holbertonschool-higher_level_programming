0666035ca3faf5f9fad4d36719e42ea90d6fb317
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-22 13:12:39 +0000

    S

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,9 +3,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    # Yeni siyahı yaradıb idx istisna olmaqla elementləri əlavə edirik
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-    return new_list
+    # Yeni siyahıya idx-dəki elementi atlayaraq əlavə edirik
+    return [my_list[i] for i in range(len(my_list)) if i != idx]
