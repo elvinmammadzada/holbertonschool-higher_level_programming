1a7f2f1e2961a48e3146f10982f158263b56f8d6
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 17:33:35 +0000

    S

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Create square with the help of rectangle class """
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class for creating Square"""
+
+    def __init__(self, size):
+        """Initialize size with private atribute"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Implemented area method """
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return the print answer"""
+        return f"[Square] {self.__size}/{self.__size}"
