344f5cb9d770ae895d6209b618f8ce041feb935b
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 19:56:33 +0000

    S

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
--- Bu script hbtn_0d_usa bazasındakı bütün şəhərləri və onların aid olduğu ştatları siyahıya alır
-SELECT cities.id, cities.name, states.name AS state_name
+-- Bu script hbtn_0d_usa bazasındakı bütün şəhərləri və aid olduğu ştatları siyahıya alır
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
