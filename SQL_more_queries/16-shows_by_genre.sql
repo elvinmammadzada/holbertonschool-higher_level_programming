d62e743cad44e304b1099b87f9b957de93530616
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 19:48:08 +0000

    S

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Bu script hbtn_0d_tvshows bazasındakı bütün şouları və onlara aid janrları siyahıya alır
+-- Əgər şouya janr təyin edilməyibsə, NULL göstərilir
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_genres ON tv_show_genres.tv_genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
