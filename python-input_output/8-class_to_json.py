0f3d0a310709451d5f2a4e88c8180ce03bc7d625
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 18:02:01 +0000

    S

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Function to return dictionary description of an object for JSON serialization"""
+"""Function to return dictionary description of an object for JSON
+serialization"""
 
 
 def class_to_json(obj):
