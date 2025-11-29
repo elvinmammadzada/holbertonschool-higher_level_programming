b30a5ae4c81f307a40d944a7efc3005589cd90ff
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 18:10:34 +0000

    S

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Student class module"""
+""" Student module
+"""
 
 
 class Student:
-    """Defines a student with first_name, last_name, age"""
+    """ Student class
+    """
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -11,7 +13,8 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation; filter attributes if attrs is a list"""
-        if isinstance(attrs, list):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        """ Retrieves dictionary representation of Student """
+        if type(attrs) is list:
+            return {k: v for k, v in self.__dict__.items()
+                    if k in attrs}
         return self.__dict__.copy()
