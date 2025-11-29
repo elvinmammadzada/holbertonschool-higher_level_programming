c41bf0aec56c731836e7dccf18f29aee90f2c503
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 17:39:37 +0000

    S

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    """Write a string to a UTF-8 text file.
+    Return the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
