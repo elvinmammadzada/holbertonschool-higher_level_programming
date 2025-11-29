964b5f701529e728336dbe382f96020ccd2098ed
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 18:12:32 +0000

    S

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Student module with JSON serialization and reloading"""
+""" Student module
+"""
+
 
 class Student:
-    """Defines a student with first_name, last_name, and age"""
+    """ Student class
+    """
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -10,16 +13,13 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns dictionary representation of the student.
-        If attrs is a list of strings, only include those attributes.
-        """
-        student_dict = self.__dict__.copy()
-        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
-            return {k: v for k, v in student_dict.items() if k in attrs}
-        return student_dict
+        """ Retrieves dictionary representation of Student """
+        if type(attrs) is list:
+            return {k: v for k, v in self.__dict__.items()
+                    if k in attrs}
+        return self.__dict__.copy()
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the student instance from a dictionary"""
-        if isinstance(json, dict):
-            for key, value in json.items():
-                setattr(self, key, value)
+        """ Replaces all attributes of the Student instance """
+        for key, value in json.items():
+            setattr(self, key, value)
