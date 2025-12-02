bfd0ba1b0f9ae5f18f861d5b72432ef130313ba1
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-12-02 12:03:20 +0400

    s

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-
 """
-Fetch https://intranet.hbtn.io/status using urllib.
+Fetches https://intranet.hbtn.io/status using urllib.
 """
 
 import urllib.request
 
-url = "https://intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response :
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
 
-    body = response.read()
-print ("Body response:")
-print (f"\t- type: {type(body)}")
-print (f"\t- content: {body}")
-print (f"\t- utf8 content: {body.decode('utf-8')}")
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
